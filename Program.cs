5c336b3a432d28e143c631f50374b24d9b1395e9
Karina Vieira <karinamelov@gmail.com>
2024-10-22 16:38:17 -0300

    Alterações para desafio de projeto efetuadas.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", _IMEI: "21452541", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("Smartphone iphone");
+Smartphone iphone = new Iphone(numero: "789456", modelo: "Modelo 2", _IMEI: "25365241", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
